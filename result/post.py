7990f67c014919390663532d11dddd32bd13d663
WanlongCai <wanlong.cai@ufz.de>
2020-04-14 23:58:27 +0800

    Add data post py

--- result/post.py
+++ result/post.py
@@ -10,20 +10,21 @@ import math
 import pandas as pd
 import csv
 
-import beier.py as beier
+import beier
 #%% semi analytical result
-ana_rt_df_Tin = pd.read_csv('./semi_ana/Result_Tin.csv',
+ana_rt_df_Tin = pd.read_csv('./Result_Tin.csv',
                     delimiter=',',
                     index_col=[0],
                     dtype={'data_index': str})
-ana_rt_df_Tout = pd.read_csv('./semi_ana/Result_Tout.csv',
+ana_rt_df_Tout = pd.read_csv('./Result_Tout.csv',
                     delimiter=',',
                     index_col=[0],
                     dtype={'data_index': str})
 
 #%%beier
-beier_T_fluid_array = beier.xxx
-beier_time_array = beier.xxx
+beier_T_in_array = beier.T1
+beier_T_out_array = beier.T2
+beier_time_array = beier.t/3600
 
 
 
--- result/post_fig.py
+++ result/post_fig.py
@@ -10,20 +10,21 @@ import csv
 plt.rcParams.update({'figure.max_open_warning': 0})
 
 #%% fig
-data_size = 100/0.2
-xaxial = np.arange(0,data_size,1)
+data_size = 501
+xaxial = np.arange(0,100,0.2)
 #plt.figure(figsize=(6, 4), dpi = 300)
 plt.figure()
    
 #beier
-plt.plot(beier_time_array,beier_T_fluid_array,c='r',lw=1,ls='--', marker='x',markersize=4,  label= 'beier')
+plt.plot(beier_time_array,beier_T_in_array,c='r',lw=1,ls='--', marker='x',markersize=4,  label= 'beier_Tin')
+plt.plot(beier_time_array,beier_T_out_array,c='b',lw=1,ls='--', marker='x',markersize=4,  label= 'beier_Tout')
 #ana
-#plt.plot(xaxial,ana_rt_df_test.iloc[0,:]-30-273.15,c='c',lw=1,ls=':', marker='x',markersize=4,  label= 'ana BHE1 soil')
-
+plt.plot(xaxial,ana_rt_df_Tin.iloc[0,1:data_size]-273.15,c='g',lw=1,ls=':', marker='x',markersize=4,  label= 'ana_Tin')
+plt.plot(xaxial,ana_rt_df_Tout.iloc[0,1:data_size]-273.15,c='y',lw=1,ls=':', marker='x',markersize=4,  label= 'ana_Tin')
 #    plt.grid(True, linestyle = "-.", color = "gray", linewidth = "0.5")
 #    plt.axhline(-5, c ='k',ls='--',lw=1, )
-plt.xlim([0,100])
-plt.ylim([10,30])
+plt.xlim([0,10])
+plt.ylim([20,45])
 #plt.yticks(np.arange(7, 12, 0.5))
 #plt.xticks(np.arange(0,data_size,2),np.arange(0,191,20))
 plt.ylabel('Temperature [$^\circ$C]',fontsize=12)
